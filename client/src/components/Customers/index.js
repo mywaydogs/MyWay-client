39d0bdd9d56381417b3ac8ebc70f8d848e6dedd6
Kfir Zvi <zvikfir10@gmail.com>
2021-07-24 13:46:09 +0300

    Replaced frontend to nextjs

--- client/src/components/Customers/index.js
+++
@@ -1,3 +0,0 @@
-import Customers from "./Customers";
-
-export default Customers;
--- client/src/components/Home.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Home() {
-    return (
-        <>דף הבית</>
-    )
-}
-
-export default Home;
