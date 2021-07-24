9639caa0dee5899622fe293aa5f724611d5b4207
Kfir Zvi <zvikfir10@gmail.com>
2021-07-24 13:38:08 +0300

    Replaced old backend with new nestjs backend

--- server/src/config/index.js
+++
@@ -1,7 +0,0 @@
-process.env.NODE_ENV = process.env.NODE_ENV || "developnent";
-
-module.exports = {
-  port: parseInt(process.env.PORT, 10),
-  databaseURL: process.env.MONGODB_URI,
-  sessionSecret: process.env.SESSION_SECRET,
-};
